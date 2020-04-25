b30cc9a52b70aa21e1eb0b0efa6805d32cdb1e0f
Danny North <lds.north@gmail.com>
2020-04-25 01:37:48 -0600

    Move styles, modify styling

--- client/src/components/WinTracker.js
+++ client/src/components/WinTracker.js
@@ -2,46 +2,10 @@ import React, { useState, useEffect } from 'react'
 
 import './WinTracker.css'
 
-import { useInterval } from './custom-hooks.util'
+import { organizeTrackerData } from '../util/tracker-utils.util'
+import { useInterval } from '../util/custom-hooks.util'
 
 const defaultState = { wins: 0, losses: 0, winStreak: 0, lossStreak: 0, rawData: {} }
-
-const getConsecutive = (arr, predicate) => {
-    const streak = arr.reduce((acc, item) => {
-        if(!acc.streakBroken) {
-            if(item === predicate) {
-                return { ...acc, count: acc.count + 1 }
-            } else {
-                return { ...acc, streakBroken: true }
-            }
-        } else {
-            return acc
-        }
-    }, { count: 0, streakBroken: false })
-
-    return streak.count || 0
-}
-
-const organizeTrackerData = (trackerData, newResponse) => {
-    if (newResponse.GameID === -1 || trackerData.rawData[newResponse.GameID] !== undefined) {
-        return trackerData;
-    }
-
-    const newRawData = {
-        ...trackerData.rawData,
-        [newResponse.GameID]: newResponse.LocalPlayerWon
-    }
-
-    const recentToOldestGames = [...Object.values(newRawData)].reverse();
-
-    return {
-        wins: newResponse.LocalPlayerWon ? trackerData.wins + 1 : trackerData.wins,
-        losses:  newResponse.LocalPlayerWon ? trackerData.losses : trackerData.losses + 1,
-        winStreak: getConsecutive(recentToOldestGames, true),
-        lossStreak: getConsecutive(recentToOldestGames, false),
-        rawData: newRawData
-    }
-}
 
 const WinTracker = () => {
     let [trackerData, setTrackerData] = useState(defaultState);
