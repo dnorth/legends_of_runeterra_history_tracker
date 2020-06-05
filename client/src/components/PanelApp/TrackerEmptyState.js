d79569d54905512e54152bc029f7bb21c18ce979
Danny North <lds.north@gmail.com>
2020-06-05 02:08:26 -0600

    enhance empty state

--- client/src/components/PanelApp/TrackerEmptyState.js
+++ client/src/components/PanelApp/TrackerEmptyState.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import TrackerIconSvg from '../../images/tracker_icon.svg'
+import HeimerEmote from '../../images/heimer_emote.png'
 
 import './TrackerEmptyState.css';
 
 const TrackerEmptyState = () => (
     <div className="emptyStateContainer">
-        <TrackerIconSvg className="emptyStateIcon" />
+        <img src={HeimerEmote} className="emptyStateIcon" />
         <div>There are no games to show.</div>
     </div>
 )
